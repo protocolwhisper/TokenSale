50706ce576379f3e832c83ddc20f82f74512e434
Theprotocolwhisperer <chrismata10@gmail.com>
2022-10-13 15:03:54 -0500

    2 Test

--- contracts/TokenSale.sol
+++ contracts/TokenSale.sol
@@ -1,12 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface IMyERC20Token is IERC20 {
+    function mint(address to, uint256 amount) external;
+}
 
 contract TokenSale {
     uint256 public ratio;
-    address public paymentToken;
+    IMyERC20Token public paymentToken; // So here whe use the interface so we can access to the mint function
 
     constructor(uint256 _ratio, address _paymentToken) {
         ratio = _ratio;
-        paymentToken = _paymentToken;
+        paymentToken = IMyERC20Token(_paymentToken);
+    }
+
+    function buyTokens() public payable {
+        uint256 paymentReceived = msg.value;
+        uint256 amountToBeGiven = paymentReceived / ratio; // This problem is about rounding  , There's no floats
+        paymentToken.mint(msg.sender, amountToBeGiven);
     }
 }
--- test/Test.ts
+++ test/Test.ts
@@ -1,17 +1,31 @@
 
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import {TokenSale} from "../typechain-types"
+import {MyERC20Token, MyERC20Token__factory, TokenSale} from "../typechain-types"
+import { token } from "../typechain-types/@openzeppelin/contracts";
 
 const TOKEN_RATIO =5
 describe("NFT Shop", async () => {
 
     let tokenSaleContract: TokenSale;
-    let address;
+    let erc20TokenContract : MyERC20Token;
+    let deployer : SignerWithAddress;
+    let acc1 : SignerWithAddress;
+    let acc2 : SignerWithAddress;
   beforeEach(async () => {
+    [deployer , acc1 , acc2] = await ethers.getSigners()
+    const erc20TokenContractFactory = await ethers.getContractFactory("MyERC20Token")
     const tokenSaleContractFactory = await ethers.getContractFactory("TokenSale")
-    tokenSaleContract = await tokenSaleContractFactory.deploy(TOKEN_RATIO , ethers.constants.AddressZero) as TokenSale
+    erc20TokenContract = await erc20TokenContractFactory.deploy() as MyERC20Token
+    await erc20TokenContract.deployed()
+    tokenSaleContract = await tokenSaleContractFactory.deploy(TOKEN_RATIO , erc20TokenContract.address) as TokenSale
     await tokenSaleContract.deployed()
+    
+    const MINTER_ROLE = await erc20TokenContract.MINTER_ROLE()
+    const grantRoleTx = await erc20TokenContract.grantRole(MINTER_ROLE , tokenSaleContract.address)
+    await grantRoleTx.wait()
+    
   });
 
   describe("When the Shop contract is deployed", async () => {
@@ -19,28 +33,41 @@ describe("NFT Shop", async () => {
         // Ratio may have some problems in differents implementations
         const ratio = await tokenSaleContract.ratio()
         expect(ratio).to.equal(TOKEN_RATIO)
-    
     });
 
     it("uses a valid ERC20 as payment token", async () => {
       const paymentToken = await tokenSaleContract.paymentToken()
       const erc20contractfactory = await ethers.getContractFactory("MyERC20Token")
       const paymentTokenContract = erc20contractfactory.attach(paymentToken) // This attach a contract instant to that address
-      //const totalSupply = 
+      const totalSupply = await paymentTokenContract.totalSupply()
+      expect(totalSupply).to.eq(0)
     });
   });
 
   describe("When a user purchase an ERC20 from the Token contract", async () => {
+
+    const paymentAmount = ethers.utils.parseEther("1")
+
+    beforeEach(async () => {
+      const buyTokensTx = await tokenSaleContract.connect(acc1).buyTokens({value : paymentAmount}) // We can override value or max fee per Gas Here
+      await buyTokensTx.wait()
+    })
     it("charges the correct amount of ETH", async () => {
-      throw new Error("Not implemented");
+      
     });
 
     it("gives the correct amount of tokens", async () => {
-      throw new Error("Not implemented");
+      const acc1Balance = await erc20TokenContract.balanceOf(acc1.address)
+      expect(acc1Balance).to.eq(paymentAmount.div(TOKEN_RATIO))
     });
   });
 
   describe("When a user burns an ERC20 at the Token contract", async () => {
+    beforeEach(async () => {
+      const buyTokensTx = await tokenSaleContract.connect(acc1).buyTokens()
+      await buyTokensTx.wait()
+
+    })
     it("gives the correct amount of ETH", async () => {
       throw new Error("Not implemented");
     });
