b2aeb692e666424d979973ff3523dbb4d3e1b1ff
Theprotocolwhisperer <chrismata10@gmail.com>
2022-10-15 14:04:38 -0500

    Burn tokens 4 Nfts

--- contracts/TokenSale.sol
+++ contracts/TokenSale.sol
@@ -1,18 +1,39 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 interface IMyERC20Token is IERC20 {
     function mint(address to, uint256 amount) external;
+
+    function burnFrom(address from, uint256 amount) external;
 }
 
-contract TokenSale {
-    uint256 public ratio;
-    IMyERC20Token public paymentToken; // So here whe use the interface so we can access to the mint function
+interface IMyERC721Token is IERC721 {
+    function safeMint(address to, uint256 tokenId) external;
 
-    constructor(uint256 _ratio, address _paymentToken) {
+    function burn(uint256 tokenId) external;
+}
+
+contract TokenSale is Ownable {
+    uint256 public ratio;
+    uint256 public price;
+    uint256 public nftPrice;
+    IMyERC20Token public paymentToken; // So here whe use the interface so we can access to the mint function
+    IMyERC721Token public nftContract;
+    uint256 public ownerPool;
+
+    constructor(
+        uint256 _ratio,
+        uint256 _nftPrice,
+        address _paymentToken,
+        address _nftcontract
+    ) {
         ratio = _ratio;
+        nftPrice = _nftPrice;
         paymentToken = IMyERC20Token(_paymentToken);
+        nftContract = IMyERC721Token(_nftcontract);
     }
 
     function buyTokens() public payable {
@@ -23,6 +44,25 @@ contract TokenSale {
 
     function burnTokens(uint256 amount) public {
         // Patter pull over Push Pattern
-        paymentToken.transferFrom(msg.sender, address(this), amount);
+        paymentToken.burnFrom(msg.sender, amount);
+        uint256 amountToBeReturned = amount * ratio;
+        payable(msg.sender).transfer(amountToBeReturned);
+    }
+
+    function buyNft(uint256 tokenId) public {
+        paymentToken.transferFrom(msg.sender, address(this), price);
+        nftContract.safeMint(msg.sender, tokenId);
+        ownerPool += price / 2;
+    }
+
+    function burnNft(uint256 tokenId) public {
+        nftContract.burn(tokenId);
+        paymentToken.transfer(msg.sender, price / 2);
+    }
+
+    function withdraw(uint256 amount) public onlyOwner {
+        require(amount <= ownerPool);
+        ownerPool -= amount;
+        paymentToken.transfer(msg.sender, amount);
     }
 }
--- test/Test.ts
+++ test/Test.ts
@@ -3,30 +3,36 @@ import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
 import { BigNumber } from "ethers";
 import { ethers } from "hardhat";
-import {MyERC20Token, MyERC20Token__factory, TokenSale} from "../typechain-types"
+import {MyERC20Token, MyERC20Token__factory, MyERC721, TokenSale} from "../typechain-types"
 import { token } from "../typechain-types/@openzeppelin/contracts";
 
 const TOKEN_RATIO =5
+const NFT_PRICE = 0.1
+
 describe("NFT Shop", async () => {
 
     let tokenSaleContract: TokenSale;
     let erc20TokenContract : MyERC20Token;
+    let erc721TokenContract : MyERC721;
     let deployer : SignerWithAddress;
     let acc1 : SignerWithAddress;
     let acc2 : SignerWithAddress;
   beforeEach(async () => {
     [deployer , acc1 , acc2] = await ethers.getSigners()
     const erc20TokenContractFactory = await ethers.getContractFactory("MyERC20Token")
+    const erc721TokenContractFactory = await ethers.getContractFactory("MyERC721")
     const tokenSaleContractFactory = await ethers.getContractFactory("TokenSale")
     erc20TokenContract = await erc20TokenContractFactory.deploy() as MyERC20Token
     await erc20TokenContract.deployed()
-    tokenSaleContract = await tokenSaleContractFactory.deploy(TOKEN_RATIO , erc20TokenContract.address) as TokenSale
+    tokenSaleContract = await tokenSaleContractFactory.deploy(TOKEN_RATIO , NFT_PRICE , erc20TokenContract.address , erc721TokenContract.address) as TokenSale
     await tokenSaleContract.deployed()
+    erc721TokenContract = await erc721TokenContractFactory.deploy() as MyERC721 // U deploy the factory firsr NOTEEE
+    await erc721TokenContract.deployed()
     
     const MINTER_ROLE = await erc20TokenContract.MINTER_ROLE()
     const grantRoleTx = await erc20TokenContract.grantRole(MINTER_ROLE , tokenSaleContract.address)
     await grantRoleTx.wait()
-    
+    // Give minter role to the tokensalecontract from the erc721one
   });
 
   describe("When the Shop contract is deployed", async () => {
@@ -46,24 +52,6 @@ describe("NFT Shop", async () => {
   });
 
   describe("When a user purchase an ERC20 from the Token contract", async () => {
-
-    const paymentAmount = ethers.utils.parseEther("1")
-
-    beforeEach(async () => {
-      const buyTokensTx = await tokenSaleContract.connect(acc1).buyTokens({value : paymentAmount}) // We can override value or max fee per Gas Here
-      await buyTokensTx.wait()
-    })
-    it("charges the correct amount of ETH", async () => {
-      
-    });
-
-    it("gives the correct amount of tokens", async () => {
-      const acc1Balance = await erc20TokenContract.balanceOf(acc1.address)
-      expect(acc1Balance).to.eq(paymentAmount.div(TOKEN_RATIO))
-    });
-  });
-
-  describe("When a user burns an ERC20 at the Token contract", async () => {
     const paymentAmount = ethers.utils.parseEther("1")
     let balanceBeforeBuyTokens : BigNumber
     let gasFeesBuyTokens : BigNumber
@@ -74,7 +62,7 @@ describe("NFT Shop", async () => {
       const buyTokenReceipt = await buyTokensTx.wait()
       gasFeesBuyTokens = (buyTokenReceipt.gasUsed).mul(buyTokenReceipt.effectiveGasPrice)
     })
-    it("gives the correct amount of ETH", async () => {
+    it("charges the correct amount of ETH", async () => {
       const balanceAfterBuyTokens = await acc1.getBalance()
       const diff = balanceBeforeBuyTokens.sub(balanceAfterBuyTokens)
       const expectedcost = paymentAmount.add(gasFeesBuyTokens)
@@ -87,45 +75,75 @@ describe("NFT Shop", async () => {
       expect(balanceContract).to.eq(paymentAmount)
     })
 
-    it("burns the correct amount of tokens", async () => {
+    it("gives the correct amount of tokens ", async () => {
       
     });
-  });
+    describe("When a user burns an ERC20 at the Token contract ", async () => {
+      let gasFeesBurnTokens: BigNumber
+      const purchaseAmount = paymentAmount.div(TOKEN_RATIO)
+      let gassFeesApproveTokens : BigNumber
 
-  describe("When a user purchase a NFT from the Shop contract", async () => {
-    it("charges the correct amount of ETH", async () => {
-      throw new Error("Not implemented");
-    });
-
-    it("updates the owner account correctly", async () => {
-      throw new Error("Not implemented");
-    });
-
-    it("update the pool account correctly", async () => {
-      throw new Error("Not implemented");
-    });
-
-    it("favors the pool with the rounding", async () => {
-      throw new Error("Not implemented");
-    });
-  });
-
-  describe("When a user burns their NFT at the Shop contract", async () => {
-    it("gives the correct amount of ERC20 tokens", async () => {
-      throw new Error("Not implemented");
-    });
-    it("updates the pool correctly", async () => {
-      throw new Error("Not implemented");
-    });
-  });
-
-  describe("When the owner withdraw from the Shop contract", async () => {
-    it("recovers the right amount of ERC20 tokens", async () => {
-      throw new Error("Not implemented");
+      beforeEach(async () => {
+        const approveTx = await erc20TokenContract.connect(acc1).approve(tokenSaleContract.address , purchaseAmount)
+        const approveReceitp = await approveTx.wait()
+        gassFeesApproveTokens = (approveReceitp.gasUsed).mul(approveReceitp.effectiveGasPrice)
+        const burnTokensTx = await tokenSaleContract.connect(acc1).burnTokens(purchaseAmount) // We can override value or max fee per Gas Here
+        const burnTokenReceipt = await burnTokensTx.wait()
+        gasFeesBurnTokens = (burnTokenReceipt.gasUsed).mul(burnTokenReceipt.effectiveGasPrice)
+      })
+      it(" gives the correct amount of ETH", async () => {
+        const balanceAfterBurnTokens = await acc1.getBalance()
+        const diff = balanceBeforeBuyTokens.sub(balanceAfterBurnTokens)
+        const expectediff = gasFeesBurnTokens.add(gasFeesBuyTokens).add(gassFeesApproveTokens)
+        const error = diff.sub(expectediff)
+        expect(error).to.eq(0)
+      });
+  
+      it("burns the correct amount of tokens", async () => {
+        const acc1Balance = await erc20TokenContract.balanceOf(acc1.address)
+        expect(acc1Balance).to.eq(0)
+        const totalSupply = await erc20TokenContract.totalSupply()
+        expect(totalSupply).to.eq(0) // Since this fails we need to implement the burn from 
+        
+      });
     });
 
-    it("updates the owner account correctly", async () => {
-      throw new Error("Not implemented");
+    describe("When a user purchase a NFT from the Shop contract", async () => {
+      it("charges the correct amount of ETH", async () => {
+        throw new Error("Not implemented");
+      });
+  
+      it("updates the owner account correctly", async () => {
+        throw new Error("Not implemented");
+      });
+  
+      it("update the pool account correctly", async () => {
+        throw new Error("Not implemented");
+      });
+  
+      it("favors the pool with the rounding", async () => {
+        throw new Error("Not implemented");
+      });
     });
+
+    describe("When a user burns their NFT at the Shop contract", async () => {
+      it("gives the correct amount of ERC20 tokens", async () => {
+        throw new Error("Not implemented");
+      });
+      it("updates the pool correctly", async () => {
+        throw new Error("Not implemented");
+      });
+    });
+  
+    describe("When the owner withdraw from the Shop contract", async () => {
+      it("recovers the right amount of ERC20 tokens", async () => {
+        throw new Error("Not implemented");
+      });
+  
+      it("updates the owner account correctly", async () => {
+        throw new Error("Not implemented");
+      });
+    });
+  
   });
 });
